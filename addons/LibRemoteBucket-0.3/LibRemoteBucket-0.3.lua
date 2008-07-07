736a6edbc3eabeb8aafc6cee916365ce6a107c49
Chorizotarian <Chorizotarian@a2206843-174d-0410-a5f4-c138417c09e6>
2008-07-07 14:50:18 +0000

    Removed file/folder

--- addons/LibRemoteBucket-0.3/LibRemoteBucket-0.3.lua
+++
@@ -1,6 +0,0 @@
-local MAJOR, MINOR = "LibRemoteBucket-0.3", 1
-local LibRemoteBucket = LibStub:NewLibrary(MAJOR, MINOR)
-if not LibRemoteBucket then return end
-
-LibRemoteBucket.embeds = LibRpc.embeds or {}
-return
