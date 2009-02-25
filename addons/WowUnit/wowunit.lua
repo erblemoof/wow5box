da78743fdb609a3dae71ed2c056d6a14473416ec
Chorizotarian <Chorizotarian@a2206843-174d-0410-a5f4-c138417c09e6>
2009-02-25 10:58:45 +0000

    WoW unit test code, mostly stolen from LuaUnit and now deleted Ace3 unit test framework

---
+++ addons/WowUnit/wowunit.lua
@@ -0,0 +1,38 @@
+require("luaunit")
+
+function assertEmpty(arg)
+    assertEquals(#arg, 0)
+end
+
+function assertNotEmpty(arg)
+    if #arg > 0 then
+        error("Value not empty", 2)
+    end
+end
+
+function assertTrue(arg)
+    if arg ~= true then
+        error("Value not true", 2)
+    end
+end
+
+function assertFalse(arg)
+    if arg ~= false then
+        error("Value not false", 2)
+    end
+end
+
+function assertNil(arg)
+    if arg ~= nil then
+        error("Value not nil", 2)
+    end
+end
+
+WowUnit = {}
+
+function WowUnit:LoadLibs(libs, libRoot)
+    libRoot = libRoot or "../libs/"
+    for _,v in pairs(libs) do
+        dofile(libRoot .. v .. "/" .. v .. ".lua")
+    end
+end
